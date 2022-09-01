640a4a9d317e0be199006ecec3c54bffb4b4c3f9
Jono Shields <jonathonshields@gmail.com>
2022-09-01 16:23:37 +1200

    move from utterances to giscus

--- components/Comments/Comments.tsx
+++ components/Comments/Comments.tsx
@@ -10,13 +10,24 @@ const Comments: React.FC = () => {
 		}
 
 		const scriptElem = document.createElement('script')
-		scriptElem.src = 'https://utteranc.es/client.js'
+		scriptElem.src = 'https://giscus.app/client.js'
 		scriptElem.async = true
 		scriptElem.crossOrigin = 'anonymous'
-		scriptElem.setAttribute('repo', 'foopod/blog6')
-		scriptElem.setAttribute('issue-term', 'url')
-		scriptElem.setAttribute('label', 'blog-comment')
-		scriptElem.setAttribute('theme', 'boxy-light')
+		scriptElem.setAttribute('data-repo', 'foopod/blog6')
+		scriptElem.setAttribute('data-repo-id', 'R_kgDOH4_ywA')
+		scriptElem.setAttribute('data-category', 'General')
+		scriptElem.setAttribute('data-category-id', 'DIC_kwDOH4_ywM4CRKZd')
+		scriptElem.setAttribute('data-mapping', 'foopod')
+		scriptElem.setAttribute('data-strict', '1')
+		scriptElem.setAttribute('data-reactions-enabled', '1')
+		scriptElem.setAttribute('data-emit-metadata', '0')
+		scriptElem.setAttribute('data-input-position', 'bottom')
+		scriptElem.setAttribute(
+			'data-theme',
+			'https://jonoshields.com/theme/style.css'
+		)
+		scriptElem.setAttribute('data-lang', 'en')
+
 		elementRef.current.appendChild(scriptElem)
 	}, [])
 
